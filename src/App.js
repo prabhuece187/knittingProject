0accf0298ceb8008b926150fd9a8d3b78b194b3a
Prabhu King <prabhuece@hotmail.com>
2023-12-21 15:40:35 +0530

    knitting model starting today

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Routes,Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Layout from './pages/layout';
+import Customer from './pages/customer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+     <Routes>
+      <Route path="/" element={<Layout />} >
+           <Route path="dashboard" element={<Dashboard />} ></Route>
+           <Route path="customer" element={<Customer />} ></Route>
+      </Route>
+     </Routes>
+    </ChakraProvider>
   );
 }
 
